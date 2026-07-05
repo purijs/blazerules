9fc1fb329e78d6f1c8d0e002e8918cb85a5be88e
Jaskaran Singh Puri <jaskaran.puri@virtualminds.com>
2026-07-05 17:18:39 +0200

    Fix wheel Python discovery for release 0.3.2

--- include/blazerules/version.h
+++ include/blazerules/version.h
@@ -3,7 +3,7 @@
 
 namespace blazerules {
 
-inline constexpr const char* VERSION = "0.3.1";
+inline constexpr const char* VERSION = "0.3.2";
 inline constexpr const char* RULE_YAML_COMPATIBILITY = "2.x";
 
 } // namespace blazerules
